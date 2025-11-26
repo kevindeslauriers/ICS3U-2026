e45a4c31f1f64815efe5392f60024ddf02a9dcb4
Kevin DesLauriers <kdeslauriers@bayviewglen.ca>
2025-11-26 14:12:16 -0500

    Number Conversions

---
+++ Unit Five - Number Systems and Input/src/NumberSystems/Conversions.java
@@ -0,0 +1,54 @@
+package NumberSystems;
+
+public class Conversions {
+    public static String decimalToBase(int decimal, int base) {
+        if (decimal == 0) {
+            return "0";
+        }
+
+        String result = "";
+
+        // All possible digit symbols up to base 16
+        String digits = "0123456789ABCDEF";
+
+        while (decimal > 0) {
+            int remainder = decimal % base; // 0 .. base-1
+
+            // Get the corresponding digit as a 1-character String
+            String digit = digits.substring(remainder, remainder + 1);
+
+            // Put it at the front of the String
+            result = digit + result;
+
+            decimal /= base;
+        }
+
+        return result;
+    }
+
+    // any base (2–16) -> decimal
+    // assumes number only contains 0–9 and A–F (uppercase)
+    public static int baseToDecimal(String number, int base) {
+        String digits = "0123456789ABCDEF";
+        int value = 0;
+
+        for (int i = 0; i < number.length(); i++) {
+            String ch = number.substring(i, i + 1); // already uppercase
+            int digit = digits.indexOf(ch); // 0..15 or -1
+
+            value = value * base + digit;
+        }
+
+        return value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decimalToBase(42, 2));   // 101010
+        System.out.println(decimalToBase(42, 8));   // 52
+        System.out.println(decimalToBase(42, 16));  // 2A
+
+        System.out.println(baseToDecimal("101010", 2)); // 42
+        System.out.println(baseToDecimal("52", 8));     // 42
+        System.out.println(baseToDecimal("2A", 16));    // 42
+    }
+}
--- Unit Four - Iteration and Repetition/src/day2/NestedForLoops.java
+++ Unit Four - Iteration and Repetition/src/day2/NestedForLoops.java
@@ -1,15 +1,33 @@
 package day2;
 
 public class NestedForLoops {
-    public static void main(String[] args){
-        int counter = 0;
-        for(int i=1; i <=10; i++){
-            for(int j = 1; j <= i; j++){
-                counter++;      // Line 8
-                System.out.println(i + " " + j + " " + counter)
-            }
+    public static void main(String[] args) {
+        // int counter = 0;
+        // for(int i=1; i <=10; i++){
+        // for(int j = 1; j <= i; j++){
+        // counter++; // Line 8
+        // System.out.println(i + " " + j + " " + counter)
+        // }
+        // }
+
+        // system.out.println(counter);
+        // }
+
+        // for (int outer = 0; outer < 10; outer++) {
+        //     for (int inner = outer; inner < 5; inner++) {
+        //         System.out.println("outer: " + outer + " inner: " + inner);
+        //     }
+        // }
+
+
+        for (int i = 0; i < 10; i++) {
+            // stuff
         }
 
-        system.out.println(counter);
+        int i = 0;
+        while (i < 10) {
+            // stuff
+            i++;
+        }
     }
 }
